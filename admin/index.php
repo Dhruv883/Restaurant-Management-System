c04bf8944700c0f4c56dfbf07d2b31f9b2887634
Dhruv <dhruvdedhia03@gmail.com>
2023-04-22 17:30:31 +0530

    admin page

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
-<?php include ('menu.php')?>
+<?php include ('menu.php');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,10 +7,28 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="./admin.css">
       <title>Document</title>
 </head>
 <body>
-      Home
+      <div class="homecontent">
+    
+      <div class="categories card">
+        <span>5</span>
+        <span>Categories</span>
+      </div>
+
+      <div class="categories card">
+        <span>5</span>
+        <span>Food Items</span>
+      </div>
+
+      <div class="categories card">
+        <span>5</span>
+        <span>Orders</span>
+      </div>
+
+      </div>
+     
 </body>
 </html>
