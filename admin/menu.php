ed294c544931e8f9f5938b9fea52657f80daa266
Dhruv <dhruvdedhia03@gmail.com>
2023-04-22 16:12:21 +0530

    admin pages

---
+++ admin/menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="./styles.css">
+      <title>Document</title>
+</head>
+<body>
+      <nav>
+      <div class="logo">
+        <span><img src="../images/translogo.png" alt="" /></span>
+      </div>
+
+      <div class="navContent">
+        <span><a href="index.php">Home</a></span>
+        <span><a href="category.php">Category</a></span>
+        <span><a href="food.php">Food</a></span>
+        <span><a href="order.php">Order</a></span>
+      </div>
+      
+    </nav>
+</body>
+</html>
